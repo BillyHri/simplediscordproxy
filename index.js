cce7fc5c9fcd2ecbcecfbc10157a2494b7521bff
Billy H <82858643+BillyHri@users.noreply.github.com>
2023-12-26 10:57:32 +0000

    third

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require("express")
-const app = express()
-const importData = require("./data.json")
-
-let port = process.env.PORT || 3000
-
-app.get("/", (req, res) => {
-    res.send("Hello, World! a")
-})
-
-app.get("/apiData", (req, res) => {
-    res.send(importData)
-})
-
-app.listen(port, () => {
-    console.log(`proxy is listening on this port: ${3000}`)
-})
+app.post('/your-endpoint', (req, res) => {
+    // Get the 'webhook' query parameter
+    const webhookParam = req.query.webhook;
+  
+    // Store 'webhookParam' as a string (you can store it wherever you need)
+    // For example, you can log it to the console
+    console.log('Webhook Parameter:', webhookParam);
+  
+    // Read the JSON body contents
+    const jsonBody = req.body;
+  
+    // Do something with the JSON data (you can store it, process it, etc.)
+    // For example, log it to the console
+    console.log('JSON Body:', jsonBody);
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: {
+  "Content-Type": "application/json"
+  },
+    body: JSON.stringify({
+    "content": jsonBody
+  }),
+    redirect: 'follow'
+  };
+  
+  fetch("https://discord.com/api/webhooks/1189159272232275998/2PnUa6pkXUtAcHoLhl2W_STkrdNZiJhs1lX866xHylMHoBM1bnVi2jO8zdsnA3yh7QgF", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  
+  
+    // Send a response
+    res.send('Received the request successfully!');
+  });
