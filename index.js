c2ec4b8b95bc42750d136d839dde7ec3468e9d53
Billy H <82858643+BillyHri@users.noreply.github.com>
2023-12-26 23:07:44 +0000

    Working version available

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const importData = require("./data.json")
 let port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! V6")
+    res.send("Hey, you're currently on my discord proxy. If you somehow manage to figure out the APIKey, just use it for free mate. note to myself though, is that my heroku expires in 2024 so get on that mate.")
 })
 
 app.get("/apiData", (req, res) => {
@@ -21,28 +21,21 @@ app.listen(port, () => {
 
 const localAPIKey = "CCvPvuWlOH_TPT"
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     // Get the 'webhook' query parameter
     const webhookParam = req.query.webhook;
-    const givenAPIKey = req.query.key;
-
-    // Store 'webhookParam' as a string (you can store it wherever you need)
-    // For example, you can log it to the console
-    console.log('Webhook Parameter:', webhookParam);
-  
-    // Read the JSON body contents
     const jsonBody = req.body.content;
-    const givenAPIKey1 = req.body.key;
-  
-    if (process.env.APIKey != JSON.stringify(givenAPIKey)) {
+    const givenAPIKey = req.body.key;
+
+    if (process.env.APIKey != givenAPIKey) {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    // Do something with the JSON data (you can store it, process it, etc.)
-    // For example, log it to the console
-    console.log('JSON Body:', jsonBody);
-  
-  var requestOptions = {
+   // if ((await fetch(webhookParam)).status != 200) {
+   //     return res.status(401).json({ error: "Invalid discord webhook. This proxy is currently only being used for discord.com based webhooks." });
+   //  }
+
+ var requestOptions = {
     method: 'POST',
     headers: {
   "Content-Type": "application/json"
